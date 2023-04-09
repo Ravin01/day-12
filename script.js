23927178ce2d70b0e9c25e471f42c91841722fa5
Ravin G <100956485+Ravin01@users.noreply.github.com>
2023-04-09 12:32:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function get(){
+    let first = document.getElementById('first').value;
+    let second = document.getElementById('second').value;
+    let last = document.getElementById('last').value;
+    let email = document.getElementById('email').value;
+    console.log(first);
+    console.log(second);
+    console.log(last);
+    console.log(email);
+}
